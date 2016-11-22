b077bf16700b96ab7298fc394e9c7d58b9b9f4e2
Excellence Technologies <excellencetechnologies08@gmail.com>
2016-11-22 12:11:30 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,6 @@
-// config ={'shekhar_channelId':'D2W9E51FU','url':'http://excellencemagentoblog.com/slack_dev/hr/attendance/API_HR/api.php','url_chat':'https://slack.com/api/chat.postMessage'}
 config = {
     'shekhar_channelId': 'D2W9E51FU',
-    'leaveApply_API_URL': 'http://excellencemagentoblog.com/slack_dev/hr/attendance/API_HR/api.php',
-    'url': 'http://excellencemagentoblog.com/slack_dev/hr/attendance/API_HR/api.php',
+    'leaveApply_API_URL': 'http://excellencemagentoblog.com/hr/attendance/API_HR/api.php',
+    'url': 'http://excellencemagentoblog.com/hr/attendance/API_HR/api.php',
     'url_chat': 'https://slack.com/api/chat.postMessage'
 };
