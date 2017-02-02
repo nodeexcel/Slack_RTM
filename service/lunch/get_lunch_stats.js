d39ace6275ebbed29f6344d2d427e7606dec3c87
shekhar <shekhar@excellencetechnologies.in>
2017-02-02 12:17:32 +0530

    fix issues and display string in get_lunch_stats

--- service/lunch/get_lunch_stats.js
+++ service/lunch/get_lunch_stats.js
@@ -7,19 +7,26 @@ imports('config/index');
 
 exports._get_lunch_stats = function (message, dm, id, rtm, user, _subCommand, callback) {
     url = config.leaveApply_API_URL;
-    paramaters = {"action": _subCommand, "userslack_id": user.id};
+    paramaters = {"action": _subCommand, "userslack_id": 'U0FJQUFMG'};
     request_send.cancel(message, paramaters, url, function (response, error, msg) {
+        var records = '';
         if (error) {
             rtm.sendMessage('oops! some error occured', dm.id);
+            _session.destroy(id, rtm, "You have not completed your task successfully!! Please use 'help' to see all options.");
         } else {
             var p = JSON.parse(response);
             if (p.error == 1) {
-                rtm.sendMessage(user.name + '! ' + p.data.message, dm.id);
+                if (p.data.message) {
+                    rtm.sendMessage(user.name + '! ' + p.data.message, dm.id);
+                } else {
+                    rtm.sendMessage(user.name + '! ' + p.message, dm.id);
+                }
                 _session.destroy(id, rtm, "You have not completed your task successfully!! Please use 'help' to see all options.");
             } else {
                 _.forEach(p.data, function (value, key) {
-                    rtm.sendMessage(value, dm.id);
+                    records = records + value + '\n';
                 });
+                rtm.sendMessage(records, dm.id);
                 _session.destroy(id, rtm, 'You have completed your task successfully!!');
             }
         }
