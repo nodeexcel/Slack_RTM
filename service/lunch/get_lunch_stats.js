b882717c27e9c76ebbd637b162545c76ce3b7572
shekhar <shekhar@excellencetechnologies.in>
2017-02-02 12:18:59 +0530

    chnge the id

--- service/lunch/get_lunch_stats.js
+++ service/lunch/get_lunch_stats.js
@@ -7,7 +7,7 @@ imports('config/index');
 
 exports._get_lunch_stats = function (message, dm, id, rtm, user, _subCommand, callback) {
     url = config.leaveApply_API_URL;
-    paramaters = {"action": _subCommand, "userslack_id": 'U0FJQUFMG'};
+    paramaters = {"action": _subCommand, "userslack_id": user.id};
     request_send.cancel(message, paramaters, url, function (response, error, msg) {
         var records = '';
         if (error) {
