8b859eaa062a9dfd6e810207bfd93db607314967
nodeexcel <vaibhav.excel2011@gmail.com>
2017-07-24 19:27:03 +0530

    lunch fixed in api

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 config = {
     'shekhar_channelId': 'D2W9E51FU',
-    // 'leaveApply_API_URL': 'https://hr.excellencetechnologies.in/attendance/API_HR/api.php',
-    // 'url': 'https://hr.excellencetechnologies.in/attendance/API_HR/api.php',
-    'leaveApply_API_URL': 'http://dev.hr.excellencetechnologies.in/hr/attendance/API_HR/api.php',
-    'url': 'http://dev.hr.excellencetechnologies.in/hr/attendance/API_HR/api.php',
+    'leaveApply_API_URL': 'https://hr.excellencetechnologies.in/attendance/API_HR/api.php',
+    'url': 'https://hr.excellencetechnologies.in/attendance/API_HR/api.php',
+    // 'leaveApply_API_URL': 'http://dev.hr.excellencetechnologies.in/hr/attendance/API_HR/api.php',
+    // 'url': 'http://dev.hr.excellencetechnologies.in/hr/attendance/API_HR/api.php',
     'url_chat': 'https://slack.com/api/chat.postMessage'
 };
